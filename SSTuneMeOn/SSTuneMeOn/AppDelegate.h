2c6ca90126e9780ecec55ecbc9cbc0609b9c76ee
Eric John Adamos <netzon.eric@gmail.com>
2014-03-10 21:07:36 +0800

    App delegate: Initial code cleanup

--- SSTuneMeOn/SSTuneMeOn/AppDelegate.h
+++ SSTuneMeOn/SSTuneMeOn/AppDelegate.h
@@ -2,14 +2,15 @@
 //  AppDelegate.h
 //  SSTuneMeOn
 //
-//  Created by Eric John Adamos on 3/10/14.
+//  Created by Eric John Adamos <ericjohnadamos@gmail.com> on 3/10/14.
 //  Copyright (c) 2014 Eric John Adamos. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UIWindow* window;
 
 @end
