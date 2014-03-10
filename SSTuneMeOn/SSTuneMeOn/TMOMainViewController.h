3be2da6de940cf6cbc4be324598333db1b43b121
Eric John Adamos <netzon.eric@gmail.com>
2014-03-10 21:24:31 +0800

    Main view controller: Create preliminary implementation

---
+++ SSTuneMeOn/SSTuneMeOn/TMOMainViewController.h
@@ -0,0 +1,14 @@
+//
+//  TMOMainViewController.h
+//  SSTuneMeOn
+//
+//  Created by Eric John Adamos <ericjohnadamos@gmail.com> on 3/10/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TMOMainViewController : UIViewController
+
+@end
---
+++ SSTuneMeOn/SSTuneMeOn/TMOMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  TMOMainViewController.m
+//  SSTuneMeOn
+//
+//  Created by Eric John Adamos <ericjohnadamos@gmail.com> on 3/10/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import "TMOMainViewController.h"
+
+
+@interface TMOMainViewController ()
+
+@end
+
+
+@implementation TMOMainViewController
+
+#pragma mark - Memory management
+
+- (void) didReceiveMemoryWarning
+{
+  [super didReceiveMemoryWarning];
+}
+
+- (void) dealloc
+{
+  [super dealloc];
+}
+
+#pragma mark - Application lifecycle
+
+- (void) viewDidLoad
+{
+  [super viewDidLoad];
+}
+
+@end
