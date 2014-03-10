0fb6277f6ef39ddc8355d375fde2c3873f62bfb8
Eric John Adamos <netzon.eric@gmail.com>
2014-03-10 21:34:25 +0800

    App delegate: Implement application lifecycle

--- SSTuneMeOn/SSTuneMeOn/AppDelegate.m
+++ SSTuneMeOn/SSTuneMeOn/AppDelegate.m
@@ -24,19 +24,24 @@
   [super dealloc];
 }
 
+
+#pragma mark - Application lifecycle
+
 - (BOOL)          application: (UIApplication*) application
 didFinishLaunchingWithOptions: (NSDictionary*)  launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:
-                   [[UIScreen mainScreen] bounds]];
+  self.window = [[UIWindow alloc] initWithFrame:
+                 [[UIScreen mainScreen] bounds]];
   
-    /* Override point for customization after application launch. */
+  /* Override point for customization after application launch. */
   
-    self.window.backgroundColor = [UIColor whiteColor];
+  self.window.backgroundColor = [UIColor whiteColor];
   
-    [self.window makeKeyAndVisible];
+  [self.window makeKeyAndVisible];
   
-    return YES;
+  self.window.rootViewController = self.splashController;
+  
+  return YES;
 }
 
 - (void) applicationWillResignActive: (UIApplication*) application
