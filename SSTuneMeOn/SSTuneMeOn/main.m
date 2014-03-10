356601c6ccdba35151789d18c7582b7d8e47de8d
Eric John Adamos <netzon.eric@gmail.com>
2014-03-10 21:03:57 +0800

    Project file: Initial commit

---
+++ SSTuneMeOn/SSTuneMeOn/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SSTuneMeOn
+//
+//  Created by Eric John Adamos on 3/10/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
---
+++ SSTuneMeOn/SSTuneMeOnTests/SSTuneMeOnTests.m
@@ -0,0 +1,34 @@
+//
+//  SSTuneMeOnTests.m
+//  SSTuneMeOnTests
+//
+//  Created by Eric John Adamos on 3/10/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SSTuneMeOnTests : XCTestCase
+
+@end
+
+@implementation SSTuneMeOnTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
