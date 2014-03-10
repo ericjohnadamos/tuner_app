803cabb4a96af48b9db12cb98cce914fdfacae2b
Eric John Adamos <ericjohnadamos@gmail.com>
2014-03-10 21:46:43 +0800

    Main: Code cleanup

--- SSTuneMeOn/SSTuneMeOn/main.m
+++ SSTuneMeOn/SSTuneMeOn/main.m
@@ -2,7 +2,7 @@
 //  main.m
 //  SSTuneMeOn
 //
-//  Created by Eric John Adamos on 3/10/14.
+//  Created by Eric John Adamos <ericjohnadamos@gmail.com> on 3/10/14.
 //  Copyright (c) 2014 Eric John Adamos. All rights reserved.
 //
 
@@ -10,9 +10,14 @@
 
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[])
+
+int main(int argc, char* argv[])
 {
-  @autoreleasepool {
-      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  @autoreleasepool
+  {
+      return UIApplicationMain(argc,
+                               argv,
+                               nil,
+                               NSStringFromClass([AppDelegate class]));
   }
 }
