3c337c15d3658dde7c22aa4f08ab572f6e1f73c0
Eric John Adamos <netzon.eric@gmail.com>
2014-03-10 21:22:11 +0800

    Splash view controller: Create preliminary implementation

---
+++ SSTuneMeOn/SSTuneMeOn/TMOSplashViewController.m
@@ -0,0 +1,38 @@
+//
+//  TMOSplashViewController.m
+//  SSTuneMeOn
+//
+//  Created by Eric John Adamos <ericjohnadamos@gmail.com> on 3/10/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import "TMOSplashViewController.h"
+
+
+@interface TMOSplashViewController ()
+
+@end
+
+
+@implementation TMOSplashViewController
+
+#pragma mark - Memory management
+
+- (void) didReceiveMemoryWarning
+{
+  [super didReceiveMemoryWarning];
+}
+
+- (void) dealloc
+{
+  [super dealloc];
+}
+
+#pragma mark - Application lifecycle
+
+- (void) viewDidLoad
+{
+  [super viewDidLoad];
+}
+
+@end
