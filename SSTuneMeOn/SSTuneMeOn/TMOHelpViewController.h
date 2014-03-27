56bdd675fffac87cce74df3bdfb6a9a1ae1d9d5c
Eric John Adamos <ericjohnadamos@gmail.com>
2014-03-28 01:55:14 +0800

    Help view controller: Create preliminary implementation

---
+++ SSTuneMeOn/SSTuneMeOn/TMOHelpViewController.h
@@ -0,0 +1,13 @@
+//
+//  TMOHelpViewController.h
+//  SSTuneMeOn
+//
+//  Created by Eric John Adamos on 3/28/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMOHelpViewController : UIViewController
+
+@end
