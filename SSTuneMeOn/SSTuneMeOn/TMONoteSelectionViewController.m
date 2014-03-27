4e6b71e4d9669f7823ef3cd8d4f432f36dd01693
Eric John Adamos <ericjohnadamos@gmail.com>
2014-03-28 01:50:02 +0800

    Note selection view controller: Create preliminary implementation

---
+++ SSTuneMeOn/SSTuneMeOn/TMONoteSelectionViewController.m
@@ -0,0 +1,49 @@
+//
+//  TMONoteSelectionViewController.m
+//  SSTuneMeOn
+//
+//  Created by Eric John Adamos on 3/28/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import "TMONoteSelectionViewController.h"
+
+@interface TMONoteSelectionViewController ()
+
+@end
+
+@implementation TMONoteSelectionViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
