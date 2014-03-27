75fdc201dffeb8dc19448b55ca73685239f37912
Eric John Adamos <ericjohnadamos@gmail.com>
2014-03-28 02:09:08 +0800

    Dancing animation view: Create preliminary implementation

---
+++ SSTuneMeOn/SSTuneMeOn/TMODancingAnimationView.m
@@ -0,0 +1,31 @@
+//
+//  TMODancingAnimationView.m
+//  SSTuneMeOn
+//
+//  Created by Eric John Adamos on 3/28/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import "TMODancingAnimationView.h"
+
+@implementation TMODancingAnimationView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
