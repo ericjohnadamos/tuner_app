94da05393c4fbfaf48732d187fe9d9b4e482ecc8
Eric John Adamos <ericjohnadamos@gmail.com>
2014-04-06 18:07:28 +0800

    Splash view: Create preliminary implementation

---
+++ SSTuneMeOn/SSTuneMeOn/TMOSplashView.h
@@ -0,0 +1,39 @@
+//
+//  TMOSplashView.h
+//  SSTuneMeOn
+//
+//  Created by Eric John Adamos <ericjohnadamos@gmail.com> on 4/6/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TMOSplashView;
+@protocol TMOSplashViewDelegate <NSObject>
+
+- (void) splashViewDidFinishRenderingViews: (TMOSplashView*) splashView;
+
+@end
+
+@interface TMOSplashView : UIView
+
+@property (nonatomic, assign) id<TMOSplashViewDelegate> delegate;
+
+@property (nonatomic, retain) NSArray* views;
+
+/* Initializer method which also sets splash views
+ *
+ * param views - An NSArray property that represents splash views
+ * return id - instance of the view
+ */
+- (id) initWithViews: (NSArray*) views;
+
+/* Insert and bring the subview in front of the main view
+ */
+- (void) loadViews;
+
+/* Start the fading animation of the splash view
+ */
+- (void) animateFadeSplashView;
+
+@end
---
+++ SSTuneMeOn/SSTuneMeOn/TMOSplashView.m
@@ -0,0 +1,117 @@
+//
+//  TMOSplashView.m
+//  SSTuneMeOn
+//
+//  Created by Eric John Adamos <ericjohnadamos@gmail.com> on 4/6/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+static const CGFloat kAnimationDuration = 0.35f;
+static const CGFloat kAnimationDelay = 2.0f;
+
+#import "TMOSplashView.h"
+
+@implementation TMOSplashView
+
+#pragma mark - Synthesize properties
+
+@synthesize delegate = m_delegate;
+@synthesize views = m_views;
+
+#pragma mark - Memory management
+
+- (void) dealloc
+{
+  self.delegate = nil;
+  
+  self.views = nil;
+  
+  [super dealloc];
+}
+
+#pragma mark - Initializer
+
+- (id) initWithViews: (NSArray*) views
+{
+  self = [super init];
+  
+  if (self != nil)
+  {
+    self.views = views;
+  }
+  
+  return self;
+}
+
+#pragma mark - Lazy loader
+
+- (NSArray*) views
+{
+  if (m_views == nil)
+  {
+    UIView* splashOne = [[UIView alloc] initWithFrame: self.bounds];
+    [splashOne autorelease];
+    
+    splashOne.backgroundColor = [UIColor blackColor];
+    
+    NSArray* views
+      = [[NSArray alloc] initWithObjects: splashOne, nil];
+    
+    m_views = views;
+  }
+  
+  return m_views;
+}
+
+- (void) loadViews
+{
+  for (id view in self.views)
+  {
+    /* Insert all image views inside the splash view */
+    if ([view isKindOfClass: [UIView class]])
+    {
+      [self addSubview: view];
+      [self bringSubviewToFront: view];
+    }
+  }
+}
+
+- (void) animateFadeSplashView
+{
+  [self decrementFadeSplashViewAtIndex: self.views.count - 1];
+}
+
+- (void) decrementFadeSplashViewAtIndex: (NSInteger) index
+{
+  if (index < 0)
+  {
+    self.views = nil;
+    
+    if (   self.delegate != nil
+        && [self.delegate respondsToSelector:
+            @selector(splashViewDidFinishRenderingViews:)])
+    {
+      [self.delegate splashViewDidFinishRenderingViews: self];
+    }
+  }
+  else if (index >= 0 && index < self.views.count)
+  {
+    UIView* splashView = self.views[index];
+    
+    [UIView animateWithDuration: kAnimationDuration
+                          delay: kAnimationDelay
+                        options: UIViewAnimationOptionTransitionNone
+                     animations: ^(void)
+     {
+       splashView.alpha = 0.0f;
+     }
+                     completion: ^(BOOL isFinished)
+     {
+       [splashView removeFromSuperview];
+       
+       [self decrementFadeSplashViewAtIndex: index - 1];
+     }];
+  }
+}
+
+@end
