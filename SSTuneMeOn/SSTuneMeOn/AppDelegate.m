8566fa77b30fd744bebfc9836be106b3a774489e
Eric John Adamos <ericjohnadamos@gmail.com>
2014-04-06 20:32:50 +0800

    App delegate: Set status bar style into light content

--- SSTuneMeOn/SSTuneMeOn/AppDelegate.m
+++ SSTuneMeOn/SSTuneMeOn/AppDelegate.m
@@ -90,6 +90,9 @@
 - (BOOL)          application: (UIApplication*) application
 didFinishLaunchingWithOptions: (NSDictionary*)  launchOptions
 {
+  [[UIApplication sharedApplication] setStatusBarStyle:
+   UIStatusBarStyleLightContent];
+  
   /* Override point for customization after application launch. */
 
   [self.window makeKeyAndVisible];
