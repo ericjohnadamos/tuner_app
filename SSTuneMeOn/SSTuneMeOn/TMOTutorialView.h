63137f404ec3553826831d60f6217c37945dd23c
Eric John Adamos <ericjohnadamos@gmail.com>
2014-04-06 18:42:39 +0800

    Tutorial view: Create preliminary implementation

---
+++ SSTuneMeOn/SSTuneMeOn/TMOTutorialView.h
@@ -0,0 +1,13 @@
+//
+//  TMOTutorialView.h
+//  SSTuneMeOn
+//
+//  Created by Eric John Adamos <ericjohnadamos@gmail.com> on 4/6/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMOTutorialView : UIView
+
+@end
---
+++ SSTuneMeOn/SSTuneMeOn/TMOTutorialView.m
@@ -0,0 +1,25 @@
+//
+//  TMOTutorialView.m
+//  SSTuneMeOn
+//
+//  Created by Eric John Adamos <ericjohnadamos@gmail.com> on 4/6/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import "TMOTutorialView.h"
+
+@implementation TMOTutorialView
+
+- (id) initWithFrame: (CGRect) frame
+{
+  self = [super initWithFrame: frame];
+  
+  if (self != nil)
+  {
+    /* TODO: Initialze here */
+  }
+  
+  return self;
+}
+
+@end
