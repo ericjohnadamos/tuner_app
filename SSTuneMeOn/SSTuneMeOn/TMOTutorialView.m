9bbcfe403d160533012dc0bc6d6995e7361bb9f8
Eric John Adamos <ericjohnadamos@gmail.com>
2014-04-06 21:25:59 +0800

    Tutorial view: Create preliminary implementation for okay button

--- SSTuneMeOn/SSTuneMeOn/TMOTutorialView.m
+++ SSTuneMeOn/SSTuneMeOn/TMOTutorialView.m
@@ -6,25 +6,33 @@
 //  Copyright (c) 2014 Eric John Adamos. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h>
+
 #import "TMOTutorialView.h"
 #import "TMOLocalizedStrings.h"
+
+
+static const CGFloat kAnimationDuration = 0.35f;
 
 
 @interface TMOTutorialView () <UIWebViewDelegate>
 
 @property (nonatomic, retain) UIWebView* tutorialWebView;
+@property (nonatomic, retain) UIButton* okayButton;
 
 @end
 
 @implementation TMOTutorialView
 
 @synthesize tutorialWebView = m_tutorialWebView;
+@synthesize okayButton = m_okayButton;
 
 #pragma mark - Memory management
 
 - (void) dealloc
 {
   self.tutorialWebView = nil;
+  self.okayButton = nil;
   
   [super dealloc];
 }
@@ -37,9 +45,12 @@
   
   if (self != nil)
   {
-    [self addSubview: self.tutorialWebView];
+    self.backgroundColor = [UIColor blackColor];
     
-    [self loadWebView];
+    [self setupComponents];
+    
+    [self addSubview: self.tutorialWebView];
+    [self addSubview: self.okayButton];
   }
   
   return self;
@@ -51,7 +62,7 @@
 {
   if (m_tutorialWebView == nil)
   {
-    UIWebView* tutorialWebView = [[UIWebView alloc] initWithFrame: self.bounds];
+    UIWebView* tutorialWebView = [[UIWebView alloc] initWithFrame: CGRectZero];
     
     tutorialWebView.delegate = self;
     tutorialWebView.opaque = NO;
@@ -77,6 +88,35 @@
   }
   
   return m_tutorialWebView;
+}
+
+- (UIButton*) okayButton
+{
+  if (m_okayButton == nil)
+  {
+    UIButton* okayButton = [UIButton buttonWithType: UIButtonTypeRoundedRect];
+    
+    NSString* text = [TMOLocalizedStrings stringForKey:
+                      kTMOTutorialOkayButtonText];
+    
+    [okayButton setTitle: text
+                forState: UIControlStateNormal];
+    
+    [okayButton setTitleColor: [UIColor whiteColor]
+                     forState: UIControlStateNormal];
+    
+    okayButton.layer.borderWidth = 1.0f;
+    okayButton.layer.borderColor = [UIColor whiteColor].CGColor;
+    okayButton.layer.cornerRadius = 5.0f;
+    
+    [okayButton addTarget: self
+                   action: @selector(didTapOkayButton)
+         forControlEvents: UIControlEventTouchUpInside];
+    
+    m_okayButton = [okayButton retain];
+  }
+  
+  return m_okayButton;
 }
 
 #pragma mark - UIWebViewDelegate method
@@ -113,6 +153,42 @@ didFailLoadWithError: (NSError*)   error
   
   [self.tutorialWebView loadHTMLString: htmlString
                                baseURL: nil];
+}
+
+- (void) setupComponents
+{
+  CGRect viewBounds = self.bounds;
+  CGPoint viewPoint = self.center;
+  CGFloat buttonWidth = 100.0f;
+  CGFloat buttonHeight = 30.0f;
+  
+  self.tutorialWebView.frame
+    = CGRectMake(0.0f,
+                 0.0f,
+                 viewBounds.size.width,
+                 viewBounds.size.height - buttonHeight);
+  
+  CGFloat webViewMaxY = CGRectGetMaxY(self.tutorialWebView.frame);
+  
+  CGRect okayButtonFrame = CGRectMake(viewPoint.x - (buttonWidth * 0.5f),
+                                      webViewMaxY,
+                                      buttonWidth,
+                                      buttonHeight);
+  self.okayButton.frame = okayButtonFrame;
+}
+
+- (void) didTapOkayButton
+{
+  [UIView animateWithDuration: kAnimationDuration
+                   animations: ^(void)
+  {
+    self.alpha = 0.0f;
+  }
+                   completion: ^(BOOL isFinished)
+  {
+    self.alpha = 1.0f;
+    self.hidden = YES;
+  }];
 }
 
 @end
