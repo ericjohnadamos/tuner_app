31be47f543c5a5a9284c251fa8e4a7723beb8b7a
Eric John Adamos <ericjohnadamos@gmail.com>
2014-04-15 00:52:53 +0800

    Theme: Create preliminary implementation

---
+++ SSTuneMeOn/SSTuneMeOn/TMOTheme.h
@@ -0,0 +1,17 @@
+//
+//  TMOTheme.h
+//  SSTuneMeOn
+//
+//  Created by Eric John Adamos <ericjohnadamos@gmail.com> on 4/15/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TMOTheme : NSObject
+
+- (void) skinButton: (UIButton*) button;
+- (void) skinHelpButton: (UIButton*) button;
+- (void) skinSelectionButton: (UIButton*) button;
+
+@end
---
+++ SSTuneMeOn/SSTuneMeOn/TMOTheme.m
@@ -0,0 +1,28 @@
+//
+//  TMOTheme.m
+//  SSTuneMeOn
+//
+//  Created by Eric John Adamos <ericjohnadamos@gmail.com> on 4/15/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import "TMOTheme.h"
+
+@implementation TMOTheme
+
+- (void) skinButton: (UIButton*) button
+{
+  /* TODO: Implement if you want to have a core design for general buttons */
+}
+
+- (void) skinHelpButton: (UIButton*) button
+{
+  /* TODO: Implement if you want to have a core design for help button */
+}
+
+- (void) skinSelectionButton: (UIButton*) button
+{
+  /* TODO: Implement if you want to have a core design for selection button */
+}
+
+@end
