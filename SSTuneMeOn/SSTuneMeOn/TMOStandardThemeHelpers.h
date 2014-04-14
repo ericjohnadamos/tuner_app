3a8d45ac8137d51dd906bc168d5e40351c1db03c
Eric John Adamos <ericjohnadamos@gmail.com>
2014-04-15 00:54:00 +0800

    Standard theme helpers: Create preliminary implementation

---
+++ SSTuneMeOn/SSTuneMeOn/TMOStandardThemeHelpers.h
@@ -0,0 +1,16 @@
+//
+//  TMOStandardThemeHelpers.h
+//  SSTuneMeOn
+//
+//  Created by Eric John Adamos <ericjohnadamos@gmail.com> on 4/15/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TMOStandardThemeHelpers : NSObject
+
+extern NSString* const kTMOStandardThemeHelpersHelpImage;
+extern NSString* const kTMOStandardThemeHelpersSelectionImage;
+
+@end
---
+++ SSTuneMeOn/SSTuneMeOn/TMOStandardThemeHelpers.m
@@ -0,0 +1,16 @@
+//
+//  TMOStandardThemeHelpers.m
+//  SSTuneMeOn
+//
+//  Created by Eric John Adamos <ericjohnadamos@gmail.com> on 4/15/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import "TMOStandardThemeHelpers.h"
+
+@implementation TMOStandardThemeHelpers
+
+NSString* const kTMOStandardThemeHelpersHelpImage = @"help";
+NSString* const kTMOStandardThemeHelpersSelectionImage = @"notes";
+
+@end
