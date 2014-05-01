7eb9abad6a8ba5621b35711518fce462601456e6
Kevin Bernard R. San Gaspar <kevni111@gmail.com>
2014-05-01 22:26:11 +0800

    UIColor+HexString: Initial code drop

---
+++ SSTuneMeOn/SSTuneMeOn/UIColor+HexString.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+HexString.h
+//  SSTuneMeOn
+//
+//  Created by Kevin Bernard R. San Gaspar on 5/1/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIColor (HexString)
+
++ (UIColor*) colorWithHexString: (NSString *) hexString;
+
+@end
