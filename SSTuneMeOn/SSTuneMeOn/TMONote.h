8ad08d1bbd406e826ad6848dbf453eeab71516d2
Kevin Bernard R. San Gaspar <kevni111@gmail.com>
2014-05-03 02:02:26 +0800

    TMONote: Initial code drop

---
+++ SSTuneMeOn/SSTuneMeOn/TMONote.h
@@ -0,0 +1,13 @@
+//
+//  TMONote.h
+//  SSTuneMeOn
+//
+//  Created by Kevin Bernard R. San Gaspar on 5/2/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TMONote : NSObject
+
+@end
