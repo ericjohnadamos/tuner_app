56be3c18362b0e676fee80fc80bdf46cc5fc7942
Kevin Bernard R. San Gaspar <kevni111@gmail.com>
2014-05-07 10:43:06 +0800

    TMOAnalogMeter: Initial code drop

---
+++ SSTuneMeOn/SSTuneMeOn/TMOAnalogMeterView.h
@@ -0,0 +1,13 @@
+//
+//  TMOAnalogMeterView.h
+//  SSTuneMeOn
+//
+//  Created by Kevin Bernard R. San Gaspar on 5/4/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMOAnalogMeterView : UIView
+
+@end
---
+++ SSTuneMeOn/SSTuneMeOn/TMOAnalogMeterView.m
@@ -0,0 +1,31 @@
+//
+//  TMOAnalogMeterView.m
+//  SSTuneMeOn
+//
+//  Created by Kevin Bernard R. San Gaspar on 5/4/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import "TMOAnalogMeterView.h"
+
+@implementation TMOAnalogMeterView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
