b5ebef52b7bc6d769576c92981d06aaa01b4f5e4
Kevin Bernard R. San Gaspar <kevni111@gmail.com>
2014-05-10 13:03:34 +0800

    TMOTunerViewController: Initial code drop

---
+++ SSTuneMeOn/SSTuneMeOn/TMOTunerViewController.h
@@ -0,0 +1,13 @@
+//
+//  TMOTunerViewController.h
+//  SSTuneMeOn
+//
+//  Created by Kevin Bernard R. San Gaspar on 5/8/14.
+//  Copyright (c) 2014 Eric John Adamos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMOTunerViewController : UIViewController
+
+@end
